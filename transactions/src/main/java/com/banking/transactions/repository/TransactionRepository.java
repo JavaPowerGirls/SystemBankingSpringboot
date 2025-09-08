1eed256e76166863f44fd047dc67272466283ac9
username <u20211f955@upc.edu.pe>
2025-09-08 11:38:23 -0500

    fix: Correct API request mapping path

--- transactions/src/main/java/com/banking/transactions/repository/TransactionRepository.java
+++ transactions/src/main/java/com/banking/transactions/repository/TransactionRepository.java
@@ -8,7 +8,6 @@ import com.banking.transactions.model.Transaction;
 import reactor.core.publisher.Flux;
 
 
-//No se si esto es asi, voy a ver la clase
 @Repository
 public interface TransactionRepository extends ReactiveMongoRepository<Transaction, String> {
     Flux<Transaction> findBySourceAccountIdOrderByDate(String sourceAccountId);
