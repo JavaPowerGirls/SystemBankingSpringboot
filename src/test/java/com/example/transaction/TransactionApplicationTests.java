3678630b95e9d9c7f0551478c7e0fac62b2b6cd0
karenmelisa <82112655+karenmelisa@users.noreply.github.com>
2025-09-08 01:16:33 -0500

    add Transaction.pom.xml

--- src/test/java/com/example/transaction/TransactionApplicationTests.java
+++ src/test/java/com/example/transaction/TransactionApplicationTests.java
@@ -1,10 +1,10 @@
-package com.account_ms;
+package com.example.transaction;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AccountMsApplicationTests {
+class TransactionApplicationTests {
 
     @Test
     void contextLoads() {
